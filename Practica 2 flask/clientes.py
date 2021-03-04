3e25c1db23bccbdf6b7ed348a5e9cf05360ea40f
LiiR0y <73493821+LiiR0y@users.noreply.github.com>
2021-03-04 07:13:46 -0400

    Delete Practica 2 flask directory

--- Practica 2 flask/clientes.py
+++
@@ -1,8 +0,0 @@
-datos = [
-        (1,'Marcos','jimenez','809-557-9825',"marcji@gmail.com",'Santiago'),
-        (2,'Lucia', 'Medina','809-894-8781', 'lucimed@gmail.com','San Juan de la Maguana'),
-        (3,'Amelia', 'Mateo','849-298-9878', 'mateolia@gmail.com','Barahona'),
-        (4,'Lorenzo', 'Boyer','809-270-6835', 'lorenzboi@gmail.com','Santo Domingo'),
-        (5,'Esteban', 'Ramirez','829-957-4175', 'esram@gmail.com','La Vega')
-
-]
